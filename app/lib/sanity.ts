e4a2f3727a5de495dc03447c959420d322778344
Jamal Salim <jamalsalim.js12@gmail.com>
2024-09-14 09:28:59 +0000

    Added vercel ignore

--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { createClient } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
 
--- app/lib/types.ts
+++ app/lib/types.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 export type SimpleBlogCard = {
   title: string;
   smallDescription: string;
